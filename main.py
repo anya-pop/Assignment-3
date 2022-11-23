ed516e4d85b719fc51a4d702a530e0ae9453cc6a
Anya_Popova <limpopova218@gmail.com>
2022-11-23 14:47:17 -0500

    class Bank

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ class Account:
 
 class SavingsAccount(Account): #as the savings and chequing account ARE types of account, inheritence is used.
     _minimumBalance = 0
-    def __init__(self):
-        pass
     def withdraw():
         pass
 
@@ -34,21 +32,29 @@ class ChequingAccount(Account):
         pass
 
 class Bank:
-    _bankName = ''
-    def openAccount():
-        pass
-    def SearchAccount():
-        pass
+    _bankName = 'GWO'
+    def names():
+        anya = Account(1234567, 'Anya', 4, SavingsAccount(1020),ChequingAccount(500)) #instances of the class Account
+        iman = Account(2345678, 'Iman', 4, SavingsAccount(3000),ChequingAccount(1200))
+        aleks = Account(3456789, 'Aleks', 4, SavingsAccount(300),ChequingAccount(3000))
+        audrey = Account(4567890, 'Audrey', 4, SavingsAccount(4000),ChequingAccount(3000))
+        misha = Account(5678901, 'Misha', 4, SavingsAccount(4000),ChequingAccount(1000))
+        ali = Account(6789012, 'Ali', 4, SavingsAccount(500),ChequingAccount(678))
+        list = [anya, iman, aleks, audrey, misha, ali] #instances collected in a list for easier creation of the method SearchAccount
+    def SearchAccount(self):
+        number = input("Enter the number of the account you're looking for:")
+        
 
 class Program:
-    def ShowMainMenu(self):
+    def ShowMainMenu():
+        banking = Bank()
         print("Welcome to GWO bank! \nYou have the following options:") #welcome message, shows the main menu
         print("Select Account\nExit")
         mmchoice = input("Enter your option here:").lower()
         if mmchoice == "exit":
             exit()
         else:
-            Bank.SearchAccount() #USES method from the bank class
+            banking.SearchAccount() #USES method from the bank class
     def ShowAccountMenu():
         print("This is your account menu. \nYou have the following options:") #presents account information/menu
         print("Check Balance - press 1\nDeposit - press 2\nWithdraw - press 3\nExit Account - press 4")
@@ -67,5 +73,4 @@ class Program:
     def run():
         Program.ShowMainMenu()
 
-
-Program.ShowAccountMenu()
+Program.ShowMainMenu()
