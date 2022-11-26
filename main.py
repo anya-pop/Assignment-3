84899f1034c737ba40732ed352da422a5f39b470
Anya_Popova <limpopova218@gmail.com>
2022-11-26 15:20:59 -0500

    making the withdraw/deposit methods

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import site
 class Account:
-    def __init__(self, num, name, interest, balance, sav, chq):
+    def __init__(self, num, name, interest, balance):
         self.num = num
         self.name = name
         self.interest = interest
         self.balance = balance
-        self.sav=sav
-        self.chq=chq
-    def getAccountNumber(num):
-        return num
-    def getAccountHolderName(name):
-        return name
-    def getRateOfInterest(interest):
-        return interest
-    def getCurrentBalance(balance):
-        return balance
-    def deposit():
-        pass
-    def withdraw():
-        pass
+    def getAccountNumber(self):
+        return self.num
+    def getAccountHolderName(self):
+        return self.name
+    def getRateOfInterest(self):
+        return self.interest
+    def getCurrentBalance(self):
+        return self.balance
+    def deposit(self, amount):
+        print("Your current balance is now:")
+        self.balance = self.balance +amount
+        return self.balance
+    def withdraw(self, amount):
+        print("Your current balance is now:")
+        self.balance = self.balance -amount
+        return self.balance
 
 
 class SavingsAccount(Account): #as the savings and chequing account ARE types of account, inheritence is used.
@@ -33,45 +34,86 @@ class ChequingAccount(Account):
 
 class Bank:
     _bankName = 'GWO'
-    def SearchAccount(self):
-        anya = Account(1234567, 'Anya', 4, 1020,500,78) #instances of the class Account
-        iman = Account(2345678, 'Iman', 4, SavingsAccount(3000),ChequingAccount(1200))
-        aleks = Account(3456789, 'Aleks', 4, SavingsAccount(300),ChequingAccount(3000))
-        audrey = Account(4567890, 'Audrey', 4, SavingsAccount(4000),ChequingAccount(3000))
-        misha = Account(5678901, 'Misha', 4, SavingsAccount(4000),ChequingAccount(1000))
-        ali = Account(6789012, 'Ali', 4, SavingsAccount(500),ChequingAccount(678))
+    def SearchAccount(self, number):
+        anya = Account(1234567, 'Anya', 4, 1020) #instances of the class Account
+        iman = Account(2345678, 'Iman', 4, 3000)
+        aleks = Account(3456789, 'Aleks', 4, 300)
+        audrey = Account(4567890, 'Audrey', 4, 4000)
+        misha = Account(5678901, 'Misha', 4, 4000)
+        ali = Account(6789012, 'Ali', 4, 500)
         list = [anya, iman, aleks, audrey, misha, ali]                                #instances collected in a list for easier creation of the method SearchAccount
-        num = input("Enter the number of the account you're looking for:")
-        for num in list:
-            Account.getAccountNumber(num)
+        list2 = []
+        for a in list:
+            n = a.getAccountNumber()
+            n = int(n)
+            list2.append(n)
+        print(list2)
+        while number not in list2: #ensures an account with this number exists
+            print("This is not a correct number.")
+            number = int(input("Try again:"))
+        if number == 1234567:
+            account = anya
+            print("This is Anya's account")
+            return account
+        elif number == 2345678:
+            print("This is Iman's account")
+            account = iman
+            return account
+        elif number == 3456789:
+            print("This is Aleks's account")
+            account = aleks
+            return account
+        elif number == 4567890:
+            print("This is Audrey's account")
+            account = audrey
+            return account
+        elif number == 5678901:
+            print("This is Misha's account")
+            account = misha
+            return account
+        elif number == 6789012:
+            print("This is Ali's account")
+            account = ali
+            return account
         
 
 class Program:
     def ShowMainMenu():
-        banking = Bank()
+        banking = Bank() #creating instance of the class bank  
         print("Welcome to GWO bank! \nYou have the following options:") #welcome message, shows the main menu
         print("Select Account\nExit")
-        mmchoice = input("Enter your option here:").lower()
-        if mmchoice == "exit":
-            exit()
-        else:
-            banking.SearchAccount() #USES method from the bank class
-    def ShowAccountMenu():
+        while True:
+            try:
+                mmchoice = input("Enter your option here:").lower()
+            except: 
+                pass
+            if mmchoice == "exit":
+               exit()
+            elif mmchoice == "select account":
+                number = int(input("Enter the number of the account you're looking for:"))
+                #USES method from the bank class
+                Program.ShowAccountMenu(banking.SearchAccount(number))
+            else:
+                print("\nInvalid option, try again.\nSelect Account / Exit\n")
+    def ShowAccountMenu(account):
         print("This is your account menu. \nYou have the following options:") #presents account information/menu
         print("Check Balance - press 1\nDeposit - press 2\nWithdraw - press 3\nExit Account - press 4")
         amchoice = input("Enter your option here:").lower()
         while amchoice != '1' and amchoice != '2' and amchoice != '3' and amchoice != '4':
             amchoice = input("Please only enter numbers from 1 to 4:")
         if amchoice == '1':
-            Account.getCurrentBalance()
+            print("Your current balance is:")
+            print(Account.getCurrentBalance(account))
         elif amchoice == '2':
-            Account.deposit()
+            amount = int(input("You chose to deposit. Enter the amount:"))
+            print(Account.deposit(account,amount))
         elif amchoice == '3':
-            Account.withdraw()
+            amount = int(input("You chose to withdraw. Enter the amount:"))
+            print(Account.withdraw(account,amount))
         elif amchoice == '4':
             Program.ShowMainMenu()
-    @classmethod #decorator to call the method easier
+
     def run():
         Program.ShowMainMenu()
 
-Program.ShowMainMenu()
+Program.run()
